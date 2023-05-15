c8c53189fe0a76a27aa432dad528303391a2cf35
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-15 19:31:35 +0600

    added routes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fiktc6e.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,17 +28,31 @@ async function run() {
     const productCollection = client.db("emaJohnDB").collection("products");
 
     app.get("/products", async (req, res) => {
-      console.log(req.query);
+      //   console.log(req.query);
       const page = parseInt(req.query.page) || 0;
       const limit = parseInt(req.query.limit) || 10;
       const skip = page * limit;
 
-      const result = await productCollection.find().skip(skip).limit(limit).toArray();
+      const result = await productCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
       res.send(result);
     });
+    
     app.get("/totalProducts", async (req, res) => {
       const result = await productCollection.estimatedDocumentCount();
       res.send({ totalProducts: result });
+    });
+
+    app.post("/productsById", async (req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map(id=>new ObjectId(id))
+      const query = { _id: { $in: (objectIds) } };
+    //   console.log(ids);
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
